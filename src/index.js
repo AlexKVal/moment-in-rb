8a4baca8019469a78e581697fb4569f9623d6407
AlexKVal <alexkval@gmail.com>
2015-08-02 19:58:02 +0300

    [added] index.html template with link to 'foundation.css'

--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@ import './styles.css'
 import React from 'react'
 import FirstComponent from './components/FirstComponent'
 
-const applicationNode = document.createElement('div')
-applicationNode.className = 'application'
-applicationNode.id = 'application'
-
-document.body.appendChild(applicationNode)
-
-React.render(<FirstComponent/>, applicationNode, () => {
+React.render(<FirstComponent/>, document.getElementById('application'), () => {
   console.log('finished mounting application')
 })
