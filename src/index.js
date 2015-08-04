a0b8e307b6109b2fa69587b9bfbc8e706a564c49
AlexKVal <alexkval@gmail.com>
2015-08-04 16:41:42 +0300

    [added] 'Panel' component. Use it in index.js

--- src/index.js
+++ src/index.js
@@ -3,40 +3,41 @@ import './styles.css'
 import React from 'react'
 import Row from './components/foundationcss/Row'
 import Column from './components/foundationcss/Column'
+import Panel from './components/foundationcss/Panel'
 
 const MainPage = React.createClass({
   render () {
     return (
       <Row>
         <Column>
-          <div className='panel callout'>
+          <Panel callout>
             <h4>Raw Date</h4>
             <p>rawDate</p>
-          </div>
-          <div className='panel'>
+          </Panel>
+          <Panel>
             <h4>Pretty Formatting</h4>
             <p>formattedDate</p>
-          </div>
-          <div className='panel callout'>
+          </Panel>
+          <Panel callout>
             <h4>Day of Week</h4>
             <p>dayOfWeek</p>
-          </div>
-          <div className='panel'>
+          </Panel>
+          <Panel>
             <h4>Day began...</h4>
             <p>hoursAgo</p>
-          </div>
-          <div className='panel callout'>
+          </Panel>
+          <Panel callout>
             <h4>Days ends in...</h4>
             <p>timeFromNow</p>
-          </div>
-          <div className='panel'>
+          </Panel>
+          <Panel>
             <h4>Calendar Time</h4>
             <p>calendarTime</p>
-          </div>
-          <div className='panel callout'>
+          </Panel>
+          <Panel callout>
             <h4>Inspired by</h4>
             <a href='http://sahatyalkabov.com/jsrecipes/#!/backend/formatting-dates'>sahatyalkabov formatting-dates</a>
-          </div>
+          </Panel>
         </Column>
       </Row>
     )
