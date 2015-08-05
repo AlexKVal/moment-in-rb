f55a0bc5964ab27fc3023c61de7b44000c252010
AlexKVal <alexkval@gmail.com>
2015-08-05 21:25:03 +0300

    [added] 'Moment.js' and usage of it on the main page

--- src/index.js
+++ src/index.js
@@ -4,35 +4,43 @@ import React from 'react'
 import Row from './components/foundationcss/Row'
 import Column from './components/foundationcss/Column'
 import PanelWithCalendar from './components/PanelWithCalendar'
+import moment from 'moment'
 
 const MainPage = React.createClass({
   render () {
+    let now = new Date()
+    let formattedDate = moment(now).format('Do MMMM YYYY, hh:mm')
+    let dayOfWeek = moment(now).format('dddd')
+    let hoursAgo = moment(now).startOf('day').fromNow()
+    let timeFromNow = moment(now).endOf('day').fromNow()
+    let calendarTime = moment(now).subtract('days', 3).calendar()
+
     return (
       <Row>
         <Column>
           <PanelWithCalendar callout>
             <h4>Raw Date</h4>
-            <p>rawDate</p>
+            <p>{now.toString()}</p>
           </PanelWithCalendar>
           <PanelWithCalendar>
             <h4>Pretty Formatting</h4>
-            <p>formattedDate</p>
+            <p>{formattedDate}</p>
           </PanelWithCalendar>
           <PanelWithCalendar callout>
             <h4>Day of Week</h4>
-            <p>dayOfWeek</p>
+            <p>{dayOfWeek}</p>
           </PanelWithCalendar>
           <PanelWithCalendar>
             <h4>Day began...</h4>
-            <p>hoursAgo</p>
+            <p>{hoursAgo}</p>
           </PanelWithCalendar>
           <PanelWithCalendar callout>
             <h4>Days ends in...</h4>
-            <p>timeFromNow</p>
+            <p>{timeFromNow}</p>
           </PanelWithCalendar>
           <PanelWithCalendar>
             <h4>Calendar Time</h4>
-            <p>calendarTime</p>
+            <p>{calendarTime}</p>
           </PanelWithCalendar>
           <PanelWithCalendar callout>
             <h4>Inspired by</h4>
